3c0cd3a567fae773ef94c3e5a2d587cbbe31dae6
gurgle <ericysim@gmail.com>
2016-10-02 10:50:45 -0400

    separated noise code from demo code

--- perlin_noise.js
+++ perlin_noise.js
@@ -1,101 +1,60 @@
 
-GRID_SIZE = 256
-SIZE = 720
+var GRID_SIZE = 8;
 
-var renderer = PIXI.autoDetectRenderer(SIZE, SIZE);
-var stage = new PIXI.Container();
-var graphics = new PIXI.Graphics();
-document.body.appendChild(renderer.view);
-stage.addChild(graphics);
-
-function init() {
-    var points = _.map(
-        _.range(SIZE),
-        function (x) {
-            return _.map(_.range(SIZE),
-                function (y) {
-                    return noise(x/72, y/72);
-                }
-            );
-        }
-    );
-    console.log("generated");
-    console.log(points[10][10]);
-    draw(points);
-    renderer.render(stage);
-}
-
-function draw(points) {
-    graphics.lineStyle(0);
-    var xl = points.length;
-    for (var x = 0; x < xl; x++) {
-        var yl = points[x].length;
-        for (var y = 0; y < yl; y++) {
-            var h = points[x][y];
-            h = (h + 1) / 2;
-            var color = [h, h, h];
-            var hex = PIXI.utils.rgb2hex(color);
-            graphics.beginFill(hex);
-            graphics.drawRect(x, y, 1, 1);
-        }
-    }
-    graphics.beginFill(0xFFFFFF);
-    graphics.lineStyle(0);
-    var l = points.length;
-    for (var i = 0; i < l; i++) {
-        graphics.drawRect(i, points[i], 1, 720 - points[i]);
-    }
-}
-
-//  returns a gradient vector (a random unit vector)
-function grad_vec() {
+function vec() {
     var theta = Math.random() * 2 * Math.PI;
     var x = Math.cos(theta);
     var y = Math.sin(theta);
     return { x: x, y: y };
 }
 
-//  calculates dot product of distance and gradient vectors
-function grad(ix, iy, x, y) {
-     var dx = x - ix;
-     var dy = y - iy;
-     return (dx * gradient[ix][iy].x + dy * gradient[ix][iy].y);
-}
-
 function fade(t) {
     return t * t * t * (t * (t * 6 - 15) + 10);
 }
 
-//  calculates perlin noise
-function noise(x, y) {
-    x = x % GRID_SIZE;
-    y = y % GRID_SIZE;
-    var ix0 = Math.floor(x);
-    var ix1 = ix0 + 1;
-    var iy0 = Math.floor(y);
-    var iy1 = iy0 + 1;
-    var fx = fade(x - ix0);
-    var fy = fade(y - iy0);
-    var n00 = grad(ix0, iy0, x, y);
-    var n01 = grad(ix0, iy1, x, y);
-    var n10 = grad(ix1, iy0, x, y);
-    var n11 = grad(ix1, iy1, x, y);
-    var n0  = lerp(n00, n01, fy);
-    var n1  = lerp(n10, n11, fy);
-    var n   = lerp(n0, n1, fx);
-    return n
-}
-
-//  linear interpolation
 function lerp(a, b, t) {
-    return a + (b - a) * t
+    return a + (b - a) * t;
 }
 
-//  init 2d list of gradient vectors
-var gradient = _.map(
-    _.range(GRID_SIZE + 1),
-    function () { return _.map(_.range(GRID_SIZE + 1), grad_vec) }
-);
+function Noise2d() {
+    // 2d array of gradients
+    this.gradients = {};
+    for (var i = 0; i < GRID_SIZE; i++) {
+        this.gradients[i] = {};
+        for (var j = 0; j < GRID_SIZE; j++) {
+            this.gradients[i][j] = vec();
+        }
+    }
 
-init();
+    // dot product function
+    this.dot = function (cx, cy, x, y) {
+        var dx = x - cx;
+        var dy = y - cy;
+        var gradient = this.gradients[cx % GRID_SIZE][cy % GRID_SIZE]
+        return (dx * gradient.x + dy * gradient.y)
+    }
+
+    // get noise value
+    this.calculate = function (x, y) {
+        x = x % GRID_SIZE;
+        y = y % GRID_SIZE;
+
+        var x0 = Math.floor(x);
+        var y0 = Math.floor(y);
+        var x1 = x0 + 1;
+        var y1 = y0 + 1;
+
+        var n00 = this.dot(x0, y0, x, y);
+        var n01 = this.dot(x0, y1, x, y);
+        var n10 = this.dot(x1, y0, x, y);
+        var n11 = this.dot(x1, y1, x, y);
+
+        var fade_x = fade(x - x0);
+        var fade_y = fade(y - y0);
+        var n0 = lerp(n00, n01, fade_y);
+        var n1 = lerp(n10, n11, fade_y);
+        var n = lerp(n0, n1, fade_x);
+        return n;
+    }
+}
 
