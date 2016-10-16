8f31e429b0baf2993d4b48df799ca2c1a55610dc
gurgle <ericysim@gmail.com>
2016-10-16 09:45:57 -0400

    erosion

--- erosion.lua
+++ erosion.lua
@@ -1,20 +1,124 @@
 
 erosion = {}
 
-local function step(terrain, rain)
-
+local function lerp(a, b, t)
+    return a + (b - a) * t
 end
 
-function erosion.rain(terrain)
-    local w = #terrain
-    local h = #terrain[1]
-    local rain = {}
-    for x = 1, w do
-        rain[x] = {}
-        for y = 1, h do
-            rain[x][y] = 1
+local function get_unit(x, y)
+    local m = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+    if m == 0 then
+        return 0, 0
+    else
+        return x / m, y / m
+    end
+end
+
+function erosion.rain(h, n)
+    for i = 1, n do
+        local drop = erosion.drop(h)
+        while drop.size > 0 do
+            erosion.step(h, drop)
         end
     end
+end
 
+function erosion.drop(elevation, x, y)
+    local x = x or love.math.random(640)
+    local y = y or love.math.random(360)
+    local drop = {
+        x = x,
+        y = y,
+        dx = 0,
+        dy = 0,
+        size = 1,
+        dirt = 0
+    }
+    return drop
+end
+
+function erosion.step(h, drop)
+    -- src
+    local src_x = drop.x
+    local src_y = drop.y
+
+    -- src corners
+    local src_ax = math.floor(src_x)
+    local src_ay = math.floor(src_y)
+    local src_bx = (src_ax + 1 - 1) % 640 + 1
+    local src_by = (src_ay + 1 - 1) % 360 + 1
+
+    -- src fade
+    local src_fx = src_x - src_ax
+    local src_fy = src_y - src_ay
+    
+    -- descent vec
+    local dxa = h[src_ax][src_ay] - h[src_bx][src_ay]
+    local dxb = h[src_ax][src_by] - h[src_bx][src_by]
+    local dx = lerp(dxa, dxb, src_fy)
+    local dya = h[src_ax][src_ay] - h[src_ax][src_by]
+    local dyb = h[src_bx][src_ay] - h[src_bx][src_by]
+    local dy = lerp(dya, dyb, src_fx)
+
+    dx, dy = get_unit(dx, dy)
+    dx = (dx + drop.dx) / 2
+    dy = (dy + drop.dy) / 2
+    
+    -- dst
+    local dst_x = (src_x + dx - 1) % 640 + 1
+    local dst_y = (src_y + dy - 1) % 360 + 1
+
+    -- dst corners
+    local dst_ax = math.floor(dst_x)
+    local dst_ay = math.floor(dst_y)
+    local dst_bx = (dst_ax + 1 - 1) % 640 + 1
+    local dst_by = (dst_ay + 1 - 1) % 360 + 1
+
+    -- dst fade
+    local dst_fx = dst_x - dst_ax
+    local dst_fy = dst_y - dst_ay
+    
+    -- src h
+    local src_ha = lerp(h[src_ax][src_ay], h[src_ax][src_by], src_fy)
+    local src_hb = lerp(h[src_bx][src_ay], h[src_bx][src_by], src_fy)
+    local src_h = lerp(src_ha, src_hb, src_fx)
+    
+    -- dst h
+    local dst_ha = lerp(h[dst_ax][dst_ay], h[dst_ax][dst_by], dst_fy)
+    local dst_hb = lerp(h[dst_bx][dst_ay], h[dst_bx][dst_by], dst_fy)
+    local dst_h = lerp(dst_ha, dst_hb, dst_fx)
+
+    -- dh
+    local dh = dst_h - src_h
+    if dh < 0 then
+        -- dn
+        local dirt = math.min(-dh, drop.size - drop.dirt)
+        drop.dirt = drop.dirt + dirt
+        h[src_ax][src_ay] =
+            h[src_ax][src_ay] - dirt * (1 - src_fx) * (1 - src_fy)
+        h[src_ax][src_by] =
+            h[src_ax][src_by] - dirt * (1 - src_fx) * src_fy
+        h[src_bx][src_ay] =
+            h[src_bx][src_ay] - dirt * src_fx * (1 - src_fy)
+        h[src_bx][src_by] =
+            h[src_bx][src_by] - dirt * src_fx * src_fy
+    else
+        -- up
+        local dirt = math.min(dh, drop.dirt)
+        drop.dirt = drop.dirt - dirt
+        h[src_ax][src_ay] =
+            h[src_ax][src_ay] + dirt * (1 - src_fx) * (1 - src_fy)
+        h[src_ax][src_by] =
+            h[src_ax][src_by] + dirt * (1 - src_fx) * src_fy
+        h[src_bx][src_ay] =
+            h[src_bx][src_ay] + dirt * src_fx * (1 - src_fy)
+        h[src_bx][src_by] =
+            h[src_bx][src_by] + dirt * src_fx * src_fy
+    end
+    drop.x = dst_x
+    drop.y = dst_y
+    drop.dx = dx
+    drop.dy = dy
+    drop.size = drop.size - 0.01
 end
 
--- main.lua
+++ main.lua
@@ -1,46 +1,104 @@
 
-require("perlin")
-require("fractal")
-require("erosion")
+require "perlin"
+require "fractal"
+require "erosion"
+inspect = require("inspect")
+
+W = 1280 / 2
+H = 720 / 2
 
 SIZE = 360 
 
 game = {}
 
+local function lerp(a, b, t)
+    return a + (b - a) * t
+end
+
 function game.init()
     local noise = fractal.noise(perlin.noise())
     _state = {}
-    _state.terrain = {}
-    for x = 1, SIZE do
-        _state.terrain[x] = {}
-        for y = 1, SIZE do
-            _state.terrain[x][y] = noise(x / (SIZE / 4), y / (SIZE / 4))
+    _state.elevation = {}
+    for x = 1, W do
+        _state.elevation[x] = {}
+        for y = 1, H do
+            _state.elevation[x][y] =
+                noise(x / (SIZE / 4), y / (SIZE / 4))
         end
     end
+    _state.drop = erosion.drop(_state.elevation)
 end
 
 function love.load()
-    love.window.setMode(720, 720)
+    love.window.setMode(1280, 720)
     game.init()
 end
 
+function love.keypressed(k)
+    if k == "q" then
+        _state.drop = erosion.drop(_state.elevation)
+    elseif k == "w" then
+        erosion.step(_state.elevation, _state.drop)
+        print("step")
+    elseif k == "e" then
+        erosion.rain(_state.elevation, 10000)
+        print("eroded")
+    end
+end
+
+function love.mousepressed(px, py, b)
+    local x = math.floor(px / 2) + 1
+    local y = math.floor(py / 2) + 1
+end
+
 function love.draw()
-    for x = 1, SIZE do
-        for y = 1, SIZE do
+    for x = 1, W do
+        for y = 1, H do
             draw_space(x, y)
         end
+    end
+
+    local cursor = _state.cursor
+    if cursor then
+        local pitch = _state.pitch[cursor.x][cursor.y]
+        local px = cursor.x * 2
+        local py = cursor.y * 2
+        love.graphics.setColor(255, 0, 0)
+        love.graphics.setLineWidth(2)
+        love.graphics.line(
+            px,
+            py,
+            px + (pitch.x * 100),
+            py + (pitch.y * 100)
+        )
     end
 end
 
 function draw_space(x, y)
-    local h = _state.terrain[x][y]
+    local h = _state.elevation[x][y]
     h = (h + 1) / 2
     local color
     if h > 0.5 then
-        color = { 255 * h, 255 * h, 255 * h }
+        color = {
+            lerp(238, 133, (h * 2) - 1),
+            lerp(232, 153, (h * 2) - 1),
+            lerp(213, 0, (h * 2) - 1),
+        }
     else
-        color =  { 38, 139, 210 }
+        color = {
+            lerp(7, 38, h * 2),
+            lerp(54, 139, h * 2),
+            lerp(66, 210, h * 2),
+        }
     end
+    if
+        math.floor(_state.drop.x) == x and
+        math.floor(_state.drop.y) == y
+    then
+        color = { 0, 255, 0 }
+    end
+    
     love.graphics.setColor(color)
     love.graphics.rectangle("fill", (x - 1) * 2, (y - 1) * 2, 2, 2)
 end
+
